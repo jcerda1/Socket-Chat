bf148023db82a799f1263142c5659ca5576e7b89
jcerda <jccer103@gmail.com>
2018-07-30 22:44:08 -0500

    io it emiting from client-server

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http)
 
 app.get('/', (req,res) => {
-  res.sendFile(__dirname + "/index.html")
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on('connection', socket => {
+  console.log('a user is connected');
+  socket.on('disconnect', () => {
+      console.log('user has disconnected');
+  });
+  socket.on('chat message', msg => {
+    console.log('message: ' + msg);
+  });
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function() {
     console.log('listining on port 3000');
-})
+});
